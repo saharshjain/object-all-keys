eec46772bc2b59be185de29c4e1c239053aa3e4b
saharshjain <saharshjain94@outlook.com>
2020-05-05 14:13:08 +0530

    using let/const instead of var

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var util = function () {}
+const util = function() {}
 util.prototype = {
-    fetchAllValues: function (obj, keyword) {
-        var value_array = []
-        if (Object.keys(obj).length <= 0) {
-            return value_array
-        }
-        var key_array = Object.keys(obj)
-        var keyword_index = key_array.indexOf(keyword)
-        for (var iterator = 0; iterator < key_array.length; iterator++) {
-            if (typeof obj[key_array[iterator]] == "object") {
-                value_array = value_array.concat(this.fetchAllValues(obj[key_array[iterator]], keyword))
-            }
-        }
-        if (keyword_index != -1) {
-            value_array = value_array.concat(obj[key_array[keyword_index]])
-        }
-        return value_array
+  fetchAllValues: function(obj, keyword) {
+    let value_array = []
+    if (Object.keys(obj).length <= 0) {
+      return value_array
     }
+    let key_array = Object.keys(obj)
+    let keyword_index = key_array.indexOf(keyword)
+    for (let iterator = 0; iterator < key_array.length; iterator++) {
+      if (typeof obj[key_array[iterator]] == "object") {
+        value_array = value_array.concat(this.fetchAllValues(obj[key_array[iterator]], keyword))
+      }
+    }
+    if (keyword_index != -1) {
+      value_array = value_array.concat(obj[key_array[keyword_index]])
+    }
+    return value_array
+  }
 }
 module.exports = {
-    util: util
+  util: util
 }
